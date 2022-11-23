2fe5c0b8158b7cdeea7b03461ea47cc2de6db978
Arnav Arvind <f20212709@hyderabad.bits-pilani.ac.in>
2022-11-23 15:45:20 +0530

    modified buy product page
    completed styling

--- src/Components/BuyProduct.js
+++ src/Components/BuyProduct.js
@@ -1,56 +1,34 @@
 import "./BuyProductStyles.css";
 import Header from "../headerComponents/Header";
 
+function Discount() {
+  return (
+    <>
+      <strike className="product_discount">
+        <span style={{ color: "black" }}>₹ 31,000</span>
+      </strike>
+      <div>
+        <span className="product_saved">You Saved:</span>{" "}
+        <span style={{ color: "#383f51", fontSize: "18px" }}>₹ 2,000</span>
+      </div>
+    </>
+  );
+}
+
 function BuyProduct() {
+  const isDiscount = true;
+
   return (
     <>
       <Header user="customer" />
       <div className="super_container">
-        <header className="header" style={{ display: "none" }}>
-          <div className="header_main">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-6 col-12 order-lg-2 order-3 text-lg-left text-right">
-                  <div className="header_search">
-                    <div className="header_search_content">
-                      <div className="header_search_form_container">
-                        <form
-                          action="#"
-                          className="header_search_form clearfix"
-                        >
-                          <div className="custom_dropdown">
-                            <div className="custom_dropdown_list">
-                              {" "}
-                              <span className="custom_dropdown_placeholder clc">
-                                All Categories
-                              </span>{" "}
-                              <i className="fas fa-chevron-down" />
-                              <ul className="custom_list clc">
-                                <li>
-                                  <a className="clc" href="#">
-                                    All Categories
-                                  </a>
-                                </li>
-                              </ul>
-                            </div>
-                          </div>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </header>
-        <div className="single_product">
+        <div className="single_product py-3">
           <div
             className="container-fluid"
             style={{
               backgroundColor: "#fff",
-              padding: "2%",
+              padding: "2rem",
               margin: "1% auto",
-              height: "100%",
             }}
           >
             <div className="row">
@@ -62,17 +40,17 @@ function BuyProduct() {
                   />
                 </div>
               </div>
-              <div className="col-lg-6 order-3">
+              <div className="col-lg-6 order-3" style={{ paddingLeft: "5%" }}>
                 <div className="product_description">
                   <nav>
                     <ol className="breadcrumb">
                       <li className="breadcrumb-item">
-                        <a href="#">Home</a>
+                        <a href="/">Home</a>
                       </li>
                       <li className="breadcrumb-item">
-                        <a href="#">Products</a>
+                        <a href="/products">Products</a>
                       </li>
-                      <li className="breadcrumb-item active">(Category)</li>
+                      <li className="breadcrumb-item active">Category</li>
                     </ol>
                   </nav>
                   <div className="product_name">Product Name</div>
@@ -80,42 +58,32 @@ function BuyProduct() {
                   <div>
                     {" "}
                     <span className="product_price">₹ 29,000</span>{" "}
-                    <strike className="product_discount">
-                      {" "}
-                      <span style={{ color: "black" }}>
-                        ₹ 31,000<span> </span>
-                      </span>
-                    </strike>{" "}
-                  </div>
-                  <div>
-                    {" "}
-                    <span className="product_saved">You Saved:</span>{" "}
-                    <span style={{ color: "black" }}>
-                      ₹ 2,000<span> </span>
-                    </span>
+                    {isDiscount && Discount()}
                   </div>
                   <hr className="singleline" />
-                  <div>
-                    {" "}
-                    <span className="product_info">
-                      EMI starts at ₹ 2,000. No Cost EMI Available
-                      <span>
-                        <br />{" "}
-                        <span className="product_info">
-                          Warranty: 6 months warranty
-                          <span>
-                            <br />{" "}
-                            <span className="product_info">
-                              7 Days easy return policy
-                              <span>
-                                <br />{" "}
-                                <span className="product_info">
-                                  7 Days easy return policy
-                                  <span>
-                                    <br />{" "}
-                                    <span className="product_info">
-                                      In Stock: 25 units sold this week
-                                      <span> </span>
+                  <div className="row">
+                    <div className="col-lg-6">
+                      {" "}
+                      <span className="product_info">
+                        EMI starts at ₹ 2,000. No Cost EMI Available
+                        <span>
+                          <br />{" "}
+                          <span className="product_info">
+                            Warranty: 6 months warranty
+                            <span>
+                              <br />{" "}
+                              <span className="product_info">
+                                7 Days easy return policy
+                                <span>
+                                  <br />{" "}
+                                  <span className="product_info">
+                                    7 Days easy return policy
+                                    <span>
+                                      <br />{" "}
+                                      <span className="product_info">
+                                        In Stock: 25 units sold this week
+                                        <span> </span>
+                                      </span>
                                     </span>
                                   </span>
                                 </span>
@@ -124,59 +92,57 @@ function BuyProduct() {
                           </span>
                         </span>
                       </span>
-                    </span>
-                  </div>
-                  <div>
-                    <div className="row">
-                      <div className="col-md-5">
-                        <div className="br-dashed">
-                          <div className="row">
-                            <div className="col-md-3 col-xs-3">
-                              {" "}
-                              <img src="https://img.icons8.com/color/48/000000/price-tag.png" />{" "}
-                            </div>
-                            <div className="col-md-9 col-xs-9">
-                              <div className="pr-info">
-                                {" "}
-                                <span className="break-all">
-                                  Get 5% instant discount + 10X rewards @
-                                  RENTOPC
-                                </span>{" "}
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="col-md-7"> </div>
+                    </div>
+                    <div
+                      className="col-lg-6"
+                      style={{
+                        borderLeft: "1px dashed black",
+                        lineHeight: "30px",
+                      }}
+                    >
+                      <span className="product_info">
+                        Seller Name:
+                        <span>
+                          <br />{" "}
+                          <span className="product_info">
+                            GSTIN:
+                            <span>
+                              <br />{" "}
+                              <span className="product_info">
+                                Seller Address:
+                                <span>
+                                  <br />{" "}
+                                  <span className="product_info">
+                                    Estimated Delivery Date:
+                                  </span>
+                                </span>
+                              </span>
+                            </span>
+                          </span>
+                        </span>
+                      </span>
                     </div>
                   </div>
                   <hr className="singleline" />
-                  <div className="order_info d-flex flex-row">
-                    <form action="#"></form>
-                  </div>
                   <div className="row">
                     <div className="col-xs-6" style={{ marginLeft: "13px" }}>
                       <div className="product_quantity">
-                        <span style={{ paddingRight: ".25rem" }}>QTY: </span>
+                        <span>QTY: </span>
                         <input
                           id="quantity_input"
                           type="number"
                           defaultValue={1}
+                          min="1"
+                          max="10"
                         />
                       </div>
                     </div>
                     <div className="col-xs-6">
                       {" "}
-                      <button
-                        type="button"
-                        className="btn btn-primary shop-button"
-                      >
+                      <button type="button" className="btn shop-button">
                         Add to Cart
                       </button>{" "}
-                      <button
-                        type="button"
-                        className="btn btn-success shop-button"
-                      >
+                      <button type="button" className="btn shop-button">
                         Buy Now
                       </button>
                     </div>

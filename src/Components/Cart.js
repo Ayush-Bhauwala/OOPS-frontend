3e186d1745a1c7aa4d3c5f897876361e1d22716b
Ayush Bhauwala <ayushbhauwala07@gmail.com>
2022-11-24 12:33:13 +0530

    Update Cart.js

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,29 +1,54 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "../headerComponents/Header";
 import axios from "axios";
 
+function CreateTable(props) {
+  const [qty, setQty] = useState(props.qty);
+  return (
+    <>
+      <tbody>
+        <tr>
+          <td>
+            <i className="fa-solid fa-trash"></i>
+          </td>
+          <td>
+            <img></img>
+          </td>
+          <td>{props.itemName}</td>
+          <td>₹{props.price}</td>
+          <td>
+            <input
+              type="number"
+              defaultValue={qty}
+              min="1"
+              max={props.maxQty}
+              id="x"
+              onChange={(e) => {
+                setQty(parseInt(e.target.value));
+              }}
+            ></input>
+          </td>
+          <td>₹{qty * props.price}</td>
+        </tr>
+      </tbody>
+    </>
+  );
+}
+
 function Cart() {
-  const [qty, setQty] = useState(2);
-  const [itemName, setItemName] = useState("");
-  const [price, setPrice] = useState(0);
-  const [imgSrc, setImgSrc] = useState("");
-  const [maxQty, setMaxQty] = useState(1);
+  const [itemsDetails, setItemsDetails] = useState([]);
   function getCart() {
     axios({
       method: "get",
       url: "http://localhost:8080/customer/getcart/1",
     }).then((res) => {
-      setQty(res.data[0].qtybought);
-      setItemName(res.data[0].itemClass.itemName);
-      setPrice(res.data[0].itemClass.price);
-      setMaxQty(res.data[0].itemClass.qty);
+      setItemsDetails(res.data);
     });
     axios({
       method: "get",
       url: "http://localhost:8080/customer/getcart/1",
     }).catch((err) => console.log(err));
   }
-  console.log(maxQty);
   useEffect(() => {
     getCart();
   }, []);
@@ -31,46 +56,27 @@ function Cart() {
   return (
     <>
       <Header user="customer" />
-      <div>
-        <h1>Your Cart</h1>
-        <table width="100%">
-          <thead>
-            <tr>
-              <td></td>
-              <td></td>
-              <td>Item Name</td>
-              <td>Price</td>
-              <td>Quantity</td>
-              <td>Subtotal</td>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <i className="fa-solid fa-trash"></i>
-              </td>
-              <td>
-                <img></img>
-              </td>
-              <td>{itemName}</td>
-              <td>₹{price}</td>
-              <td>
-                <input
-                  type="number"
-                  defaultValue={qty}
-                  min="1"
-                  max={maxQty}
-                  id="x"
-                  onChange={() =>
-                    setQty(parseInt(document.getElementById("x").value))
-                  }
-                ></input>
-              </td>
-              <td>₹{qty * price}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <h1>Your Cart</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <td></td>
+            <td></td>
+            <td>Item Name</td>
+            <td>Price</td>
+            <td>Quantity</td>
+            <td>Subtotal</td>
+          </tr>
+        </thead>
+        {itemsDetails.map((item) => (
+          <CreateTable
+            qty={item.qtybought}
+            itemName={item.itemClass.itemName}
+            price={item.itemClass.price}
+            maxQty={item.itemClass.qty}
+          />
+        ))}
+      </table>
     </>
   );
 }

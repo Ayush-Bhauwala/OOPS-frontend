b818cf9e46a0d5e300c1a12d3c62e363b6ff3511
Arnav Arvind <f20212709@hyderabad.bits-pilani.ac.in>
2022-11-29 23:56:35 +0530

    modified login page
    added labels, alert

--- src/CustomerComponents/Login.js
+++ src/CustomerComponents/Login.js
@@ -1,13 +1,21 @@
+import { useState } from "react";
 import { Link, Route } from "react-router-dom";
 import "./LoginStyles.css";
 import LoginPopup from "./LoginPopup";
 import axios from "axios";
 
 function Login() {
+  const [alert, setAlert] = useState(false);
+
   function handleClick(e) {
     e.preventDefault();
     const email = document.getElementById("email").value;
-    const password = document.getElementById("pass").value;
+    const password = document.getElementById("password").value;
+    if (password === "" || email === "") {
+      setAlert(true);
+    } else {
+      setAlert(false);
+    }
     console.log(email, password);
     axios
       .post("http://localhost:8080/login", {
@@ -29,6 +37,19 @@ function Login() {
       className="vh-100"
       style={{ backgroundColor: "#383f51", maxHeight: "100vh" }}
     >
+      {alert && (
+        <>
+          <div
+            className="alert alert-danger"
+            role="alert"
+            style={{ padding: ".5%" }}
+          >
+            <i className="fa-solid fa-triangle-exclamation"></i> All fields are
+            required!
+          </div>
+        </>
+      )}
+
       <div className="container align-items-center h-100">
         <div className="row d-flex justify-content-center align-items-center h-100">
           <div className="col col-xl-10">
@@ -43,43 +64,46 @@ function Login() {
                 </div>
                 <div className="col-md-6 col-lg-6 d-flex align-items-center">
                   <div className="card-body p-4 p-lg-3 ms-3 me-3 card-container ">
-                    <form style={{ height: "100%" }}>
-                      <div className="d-flex align-items-center mb-3 pb-1">
-                        <img
-                          className="login-logo"
-                          src="https://www.logodesign.net/logo/arrow-marketing-112ld.png"
-                          alt="logo"
-                        ></img>
-                        <span className="h1 fw-bold mb-0 login-name">
-                          Bargains
-                        </span>
-                      </div>
+                    <div className="d-flex align-items-center mb-3 pb-1">
+                      <img
+                        className="login-logo"
+                        src="https://www.logodesign.net/logo/arrow-marketing-112ld.png"
+                        alt="logo"
+                      ></img>
+                      <span className="h1 fw-bold mb-0 login-name">
+                        Bargains
+                      </span>
+                    </div>
 
-                      <h5
-                        className="fw-normal mt-4 mb-4"
-                        style={{ color: "#383f51", fontSize: "30px" }}
-                      >
-                        Sign into your account
-                      </h5>
-
-                      <div className="form-outline mb-4">
+                    <h5
+                      className="fw-normal mt-4 mb-4"
+                      style={{ color: "#383f51", fontSize: "30px" }}
+                    >
+                      Sign into your account
+                    </h5>
+                    <form>
+                      <div className="form-floating mb-4">
                         <input
                           type="email"
                           className="form-control login-input-field"
                           placeholder="Email address"
                           id="email"
                           name="email"
+                          required
                         />
+                        <label for="email">Email Address</label>
                       </div>
 
-                      <div className="form-outline mb-4">
+                      <div className="form-floating mb-4">
                         <input
                           type="password"
                           className="form-control login-input-field"
                           placeholder="Password"
-                          id="pass"
-                          name="pass"
+                          id="password"
+                          name="password"
+                          required
                         />
+                        <label for="password">Password</label>
                       </div>
 
                       <div className="pt-1 mb-4 login-div">
